d42af8ec43c962bb7df82d4dc6301554125bc6e1
Reiner Tupaz <35625548+turdmaggot@users.noreply.github.com>
2021-10-09 13:58:42 +0800

    Feature/v1.0.1 improvements (#1)
    * Added regioning, new methods.
    
    * Updated project version to 1.0.1
    
    * Added documentation for stringhelper methods
    
    * Added documentation for ReCaptcha helper.
    
    * Updated readme.
    
    * Updated to 1.0.2, changed targetframework to .net5
    
    * Changed target framework to .net 4.0
    
    Co-authored-by: Reiner Tupaz <reiner.tupaz@infor.com>

--- ReinST.Central/Helpers/StringHelper.cs
+++ ReinST.Central/Helpers/StringHelper.cs
@@ -13,6 +13,26 @@ namespace ReinST.Central.Helpers
 {
     public static class StringHelper
     {
+        #region Constants
+
+        private const string HTMLRegex = @"<(.|\n)*?>";
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Gets the first image from a string with HTML content, for sharing in Facebook.
+        /// </summary>
+        /// <param name="content">
+        /// Content to scan for a default image.
+        /// </param>
+        /// <param name="defaultImageURL">
+        /// URL to of the image to display when no image tags are found.
+        /// </param>
+        /// <returns>
+        /// URL of the first image tag found from the content.
+        /// </returns>
         public static string GetFirstImageForShare(string content, string defaultImageURL)
         {
             try
@@ -42,6 +62,18 @@ namespace ReinST.Central.Helpers
             }
         }
 
+        /// <summary>
+        /// Gets the first image from a string with HTML content, for a website front page.
+        /// </summary>
+        /// <param name="content">
+        /// Content to scan for a default image.
+        /// </param>
+        /// <param name="defaultImageURL">
+        /// URL to of the image to display when no image tags are found.
+        /// </param>
+        /// <returns>
+        /// URL of the first image tag found from the content.
+        /// </returns>
         public static string GetFirstImageForFrontPage(string content, string defaultImageURL)
         {
             try
@@ -71,6 +103,18 @@ namespace ReinST.Central.Helpers
             }
         }
 
+        /// <summary>
+        /// Gets the first image from a string with HTML content, for icons on a listview etc.
+        /// </summary>
+        /// <param name="content">
+        /// Content to scan for a default image.
+        /// </param>
+        /// <param name="defaultIconURL">
+        /// URL to of the image to display when no image tags are found.
+        /// </param>
+        /// <returns>
+        /// URL of the first image tag found from the content.
+        /// </returns>
         public static string GetFirstImageIcon(string content, string defaultIconURL)
         {
             try
@@ -100,6 +144,15 @@ namespace ReinST.Central.Helpers
             }
         }
 
+        /// <summary>
+        /// Gets all images from string with HTML content.
+        /// </summary>
+        /// <param name="inputHTML">
+        /// Content to scan for images
+        /// </param>
+        /// <returns>
+        /// Collection of images in a list.
+        /// </returns>
         public static List<string> GetAllImages(string inputHTML)
         {
             try
@@ -123,6 +176,16 @@ namespace ReinST.Central.Helpers
             }
         }
 
+        /// <summary>
+        /// Gets all images from string with HTML content, with absolute URL.
+        /// Note: You have to set root URL via "admin" app setting from web.config.
+        /// </summary>
+        /// <param name="inputHTML">
+        /// Content to scan for images
+        /// </param>
+        /// <returns>
+        /// Collection of images in a list.
+        /// </returns>
         public static List<string> GetAllImagesWithFullURL(string inputHTML)
         {
             try
@@ -146,13 +209,31 @@ namespace ReinST.Central.Helpers
             }
         }
 
-
+        /// <summary>
+        /// Trims the given string to the desired size.
+        /// </summary>
+        /// <param name="input">
+        /// String to trim
+        /// </param>
+        /// <param name="max">
+        /// Max no. of characters for the string
+        /// </param>
+        /// <returns>
+        /// Trimmed string
+        /// </returns>
         public static string TrimToMaxSize(string input, int max)
         {
             return ((input != null) && (input.Length > max)) ?
                 input.Substring(0, max) : input;
         }
 
+
+        /// <summary>
+        /// Generates a random string.
+        /// </summary>
+        /// <returns>
+        /// A random string
+        /// </returns>
         public static string RandomStr()
         {
             string rStr = Path.GetRandomFileName();
@@ -160,6 +241,18 @@ namespace ReinST.Central.Helpers
             return rStr;
         }
 
+        /// <summary>
+        /// Trims the given string to the desired size, then appends ellipsis.
+        /// </summary>
+        /// <param name="input">
+        /// String to trim
+        /// </param>
+        /// <param name="max">
+        /// Max no. of characters for the string
+        /// </param>
+        /// <returns>
+        /// Trimmed string, with ellipsis.
+        /// </returns>
         public static string CutIfLong(string input, int max)
         {
             if (input.Length > max)
@@ -173,11 +266,26 @@ namespace ReinST.Central.Helpers
             }
         }
 
+        /// <summary>
+        /// Generates a datetime timstamp string.
+        /// </summary>
+        /// <returns>
+        /// Datetime timestamp.
+        /// </returns>
         public static string GenerateDateTimeStamp()
         {
             return DateTime.Now.Year.ToString() + DateTime.Now.Month + DateTime.Now.Day + "_" + DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString() + DateTime.Now.Second.ToString() + DateTime.Now.Millisecond.ToString();
         }
 
+        /// <summary>
+        /// Generates random text based on given length
+        /// </summary>
+        /// <param name="length">
+        /// Max no. of characters for the string
+        /// </param>
+        /// <returns>
+        /// A random string.
+        /// </returns>
         public static string GenerateRandomText(int length)
         {
             try
@@ -197,7 +305,16 @@ namespace ReinST.Central.Helpers
             }
         }
 
-        private static string ExtractYoutubeID(string youtubeURL)
+        /// <summary>
+        /// Extracts the Youtube video ID from a youtube URL.
+        /// </summary>
+        /// <param name="youtubeURL">
+        /// The URL of the video from Youtube.
+        /// </param>
+        /// <returns>
+        /// The Youtube ID of the Youtube video.
+        /// </returns>
+        public static string ExtractYoutubeID(string youtubeURL)
         {
             //Setup the RegEx Match and give it 
             Match regexMatch = Regex.Match(youtubeURL, "^[^v]+v=(.{11}).*", RegexOptions.IgnoreCase);
@@ -210,6 +327,15 @@ namespace ReinST.Central.Helpers
             return youtubeURL;
         }
 
+        /// <summary>
+        /// Gets the entire HTML content from a URL.
+        /// </summary>
+        /// <param name="url">
+        /// The URL of the site to be scraped.
+        /// </param>
+        /// <returns>
+        /// Scraped HTML content of the site from the URL.
+        /// </returns>
         public static string GetWebContent(string url)
         {
             Uri uri = new Uri(url);
@@ -223,12 +349,37 @@ namespace ReinST.Central.Helpers
             return output;
         }
 
+        /// <summary>
+        /// Formats currency in Philippine format (e.g XXX,XXX,XXX.XX).
+        /// </summary>
+        /// <param name="input">
+        /// Parseable string to convert.
+        /// </param>
+        /// <returns>
+        /// Formatted currency string.
+        /// </returns>
         public static string FormatMoney(string input)
         {
-            double dInput = Convert.ToDouble(input);
-            return dInput.ToString("C", CultureInfo.CreateSpecificCulture("en-PH"));
+            try
+            {
+                double dInput = Convert.ToDouble(input);
+                return dInput.ToString("C", CultureInfo.CreateSpecificCulture("en-PH"));
+            }
+            catch
+            {
+                return string.Empty;
+            }
         }
 
+        /// <summary>
+        /// Formats to currency.
+        /// </summary>
+        /// <param name="input">
+        /// Parseable string to convert.
+        /// </param>
+        /// <returns>
+        /// Formatted currency string.
+        /// </returns>
         public static string FormatMoney(double input)
         {
             try
@@ -237,10 +388,19 @@ namespace ReinST.Central.Helpers
             }
             catch
             {
-                return "";
+                return string.Empty;
             }
         }
 
+        /// <summary>
+        /// Converts currency string with "PHP" to words.
+        /// </summary>
+        /// <param name="strInput">
+        /// Parseable amount with currency to convert.
+        /// </param>
+        /// <returns>
+        /// Currency converted to words.
+        /// </returns>
         public static string CurrencyToText(string strInput)
         {
             if (strInput == null || strInput.Length <= 0)
@@ -249,7 +409,8 @@ namespace ReinST.Central.Helpers
             }
             try
             {
-                strInput = strInput.Replace(",", "").Replace("Php", "").Trim();
+                strInput = strInput.ToLower();
+                strInput = strInput.Replace(",", string.Empty).Replace("php", string.Empty).Trim();
                 int decimalCount = 0;
                 int Val = strInput.Length - 1;
                 for (int x = 0; x <= Val; x++)
@@ -355,6 +516,172 @@ namespace ReinST.Central.Helpers
             }
         }
 
+        /// <summary>
+        /// Strips HTML tags off a given string. This uses the HTMLAgilityLibrary.
+        /// </summary>
+        /// <param name="htmlString">
+        /// Input string.
+        /// </param>
+        /// <returns>
+        /// Input string with HTML tags strpped off.
+        /// </returns>
+        public static string StripHTML(string htmlString)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(htmlString);
+
+            if (htmlDoc == null)
+                return htmlString;
+
+            return htmlDoc.DocumentNode.InnerText;
+        }
+
+        /// <summary>
+        /// Strips HTML tags off a given string. This uses regex matching.
+        /// </summary>
+        /// <param name="htmlString">
+        /// Input string.
+        /// </param>
+        /// <returns>
+        /// Input string with HTML tags strpped off.
+        /// </returns>
+        public static string StripHTMLViaRegex(string htmlString)
+        {
+            return Regex.Replace(htmlString, HTMLRegex, string.Empty);
+        }
+
+        /// <summary>
+        /// Detects if a given string has HTML tags. This uses regex matching.
+        /// </summary>
+        /// <param name="value">
+        /// Input string to check.
+        /// </param>
+        /// <returns>
+        /// Returns true if there's HTML tags in the string.
+        /// </returns>
+        public static bool ContainsHTML(string value)
+        {
+            if (value != null)
+                return Regex.IsMatch(value, HTMLRegex);
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Hashes given a given string using MD5.
+        /// </summary>
+        /// <param name="input">
+        /// Input string to hash.
+        /// </param>
+        /// <returns>
+        /// MD5 hash of the given string.
+        /// </returns>
+        public static string MD5(string input)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+
+            byte[] originalBytes = ASCIIEncoding.Default.GetBytes(input);
+            byte[] encodedBytes = md5.ComputeHash(originalBytes);
+
+            return BitConverter.ToString(encodedBytes).Replace("-", "");
+        }
+
+        /// <summary>
+        /// Hashes given a given string using BCrypt.
+        /// </summary>
+        /// <param name="input">
+        /// Input string to hash.
+        /// </param>
+        /// <returns>
+        /// BCrypt hash of the given string.
+        /// </returns>
+        public static string GenerateBCryptHash(string input)
+        {
+            //adjust salt level here below"
+            string salt = BCrypt.GenerateSalt();
+
+            return BCrypt.HashPassword(input.Trim(), salt);
+        }
+
+        /// <summary>
+        /// Verifies the BCrypt hash against an input string.
+        /// </summary>
+        /// <param name="plainText">
+        /// String in plain text to verify.
+        /// </param>
+        /// /// <param name="hash">
+        /// Hash to verify the plain text against.
+        /// </param>
+        /// <returns>
+        /// True, if the hash matches the input string.
+        /// </returns>
+        public static bool VerifyBCryptHash(string plainText, string hash)
+        {
+            return BCrypt.CheckPassword(plainText, hash);
+        }
+
+        /// <summary>
+        /// Converts numbers to words.
+        /// </summary>
+        /// <param name="number">
+        /// Number to be converted.
+        /// </param>
+        /// <returns>
+        /// Integer converted to words.
+        /// </returns>
+        public static string NumberToWords(int number)
+        {
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+
+            return words;
+        }
+
+        #endregion
+
+        #region Private Methods
+
         private static string HundredsText(string value)
         {
             try
@@ -409,40 +736,6 @@ namespace ReinST.Central.Helpers
             }
         }
 
-        public static string StripHTML(string htmlString)
-        {
-            HtmlDocument htmlDoc = new HtmlDocument();
-            htmlDoc.LoadHtml(htmlString);
-
-            if (htmlDoc == null)
-                return htmlString;
-
-            return htmlDoc.DocumentNode.InnerText;
-        }
-
-        public static string MD5(string input)
-        {
-            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-
-            byte[] originalBytes = ASCIIEncoding.Default.GetBytes(input);
-            byte[] encodedBytes = md5.ComputeHash(originalBytes);
-
-            return BitConverter.ToString(encodedBytes).Replace("-", "");
-        }
-
-        public static string GenerateBCryptHash(string input)
-        {
-            //adjust salt level here below"
-            string salt = BCrypt.GenerateSalt();
-
-            return BCrypt.HashPassword(input.Trim(), salt);
-        }
-
-        public static bool VerifyBCryptHash(string plainText, string hash)
-        {
-            return BCrypt.CheckPassword(plainText, hash);
-        }
-
         private static string[] Tens = new string[]
         {
             "Ten", "Twenty", "Thirty", "Forty", "Fifty",
@@ -455,55 +748,6 @@ namespace ReinST.Central.Helpers
             "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
         };
 
-        public static string NumberToWords(int number)
-        {
-            if (number == 0)
-                return "zero";
-
-            if (number < 0)
-                return "minus " + NumberToWords(Math.Abs(number));
-
-            string words = "";
-
-            if ((number / 1000000) > 0)
-            {
-                words += NumberToWords(number / 1000000) + " million ";
-                number %= 1000000;
-            }
-
-            if ((number / 1000) > 0)
-            {
-                words += NumberToWords(number / 1000) + " thousand ";
-                number %= 1000;
-            }
-
-            if ((number / 100) > 0)
-            {
-                words += NumberToWords(number / 100) + " hundred ";
-                number %= 100;
-            }
-
-            if (number > 0)
-            {
-                if (words != "")
-                    words += "and ";
-
-                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-
-                if (number < 20)
-                    words += unitsMap[number];
-                else
-                {
-                    words += tensMap[number / 10];
-                    if ((number % 10) > 0)
-                        words += "-" + unitsMap[number % 10];
-                }
-            }
-
-            return words;
-        }
-
-
+        #endregion
     }
 }

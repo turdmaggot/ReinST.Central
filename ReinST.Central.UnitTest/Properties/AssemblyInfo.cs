f178754f5d755bef3b1d717fb453ec8a3cd4b518
Reiner Tupaz <rstupaz@hotmail.com>
2021-10-10 20:39:21 +0800

    Added unit test project, changed assembly version.

---
+++ ReinST.Central.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReinST.Central.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReinST.Central.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c667e152-2cfe-4b60-af2b-9543b5b7d2a2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReinST.Central.UnitTest/SampleTests/Sample1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReinST.Central.Helpers;
+
+namespace ReinST.Central.UnitTest.SampleTests
+{
+    [TestClass]
+    public class Sample1
+    {
+        [TestMethod]
+        public void AuthenticateViaBCryptHash()
+        {
+            // Arrange
+            string plainText = "Hello world!";
+            string hash = StringHelper.GenerateBCryptHash(plainText);
+
+            // Act
+            bool isValid = StringHelper.VerifyBCryptHash(plainText, hash);
+
+            // Assert
+            Assert.IsTrue(isValid, plainText + " is correctly hashed!");
+        }
+
+        [TestMethod]
+        public void TestAESEncryptor()
+        {
+            // Arrange
+            string plainText = "Encrypt me!";
+
+            // Act
+            string encrypted = StringHelper.AESEncrypt(plainText);
+            string decrypted = StringHelper.AESDecrypt(encrypted);
+
+            // Assert
+            Assert.AreEqual(plainText, decrypted, plainText + " is correctly encrypted via AES!");
+        }
+
+        [TestMethod]
+        public void TestBase64Encoder()
+        {
+            // Arrange
+            string plainText = "Encode me!";
+
+            // Act
+            string encoded = StringHelper.Base64Encode(plainText);
+            string decoded = StringHelper.Base64Decode(encoded);
+
+            // Assert
+            Assert.AreEqual(plainText, decoded, plainText + " is correctly encoded in Base64!");
+        }
+    }
+}

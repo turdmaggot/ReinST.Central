7190a08f608c53442bc47986201145bec17dd4cd
reinmann <rstupaz@hotmail.com>
2021-10-26 01:13:26 +0800

    added dataaccess test

---
+++ ReinST.Central.UnitTest/DataAccessTests/DataAccessTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ReinST.Central.DataManagement;
+using System.IO;
+
+namespace ReinST.Central.UnitTest.DataAccessTests
+{
+    [TestClass]
+    public class DataAccessTest1
+    {
+        // Set DB here:
+        private const string DatabaseInstance = "localhost";
+        private const string InitialCatalog = "ReinSTCentral";
+        private const string Username = "sa";
+        private const string Password = "iamreiner";
+
+        [TestMethod]
+        public void CreateTestTableIfNotExists()
+        {
+            using (DataAccess da = new DataAccess(DatabaseInstance, InitialCatalog, Username, Password))
+            {
+                string script = File.ReadAllText(@"SQLScripts\SampleTable1.sql");
+                da.ExecuteScript(script);
+            }
+        }
+    }
+}
